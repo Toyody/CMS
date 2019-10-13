e1cdbc149ccd54c8adf7c11546ef975e3f71e81b
vagrant <vagrant@homestead>
2019-10-13 09:23:55 +0000

    シェアボタン追加

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -76,6 +76,6 @@
     <!-- Scripts -->
     <script src="{{ asset('js/page.min.js') }}"></script>
     <script src="{{ asset('js/script.js') }}"></script>
-
+    <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5da2eb3b438c1f6d"></script>
   </body>
 </html>
